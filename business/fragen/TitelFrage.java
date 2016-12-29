3014e65fd83ceb6007883ed0ff25295458b7e9b2
joshuba <joshua.barth@me.com>
2016-12-29 14:10:01 +0100

    Track object added

--- business/fragen/TitelFrage.java
+++ business/fragen/TitelFrage.java
@@ -1,10 +1,10 @@
 package hearrun.business.fragen;
 
-import hearrun.business.Lied;
+import hearrun.business.Track;
 
 /**
  * Created by Josh on 28.12.16.
  */
 public class TitelFrage extends Frage {
-    Lied song;
+    Track song;
 }
