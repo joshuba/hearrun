6b452a68cb54e41a591a4a953323e06063a1ed92
lebck <leo.back@yahoo.de>
2017-02-02 15:13:55 +0100

    CoverWahlFragen - einlesen abgesichert

--- business/fragen/CoverWahlFrage.java
+++ business/fragen/CoverWahlFrage.java
@@ -7,6 +7,7 @@ import com.mpatric.mp3agic.UnsupportedTagException;
 import hearrun.business.exceptions.TagNeededException;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.image.Image;
+import javafx.scene.image.PixelReader;
 
 import javax.imageio.ImageIO;
 import java.io.ByteArrayInputStream;
@@ -21,7 +22,7 @@ public class CoverWahlFrage extends Frage {
     private Image[] antworten;
     private String path;
 
-    public CoverWahlFrage(String fragetext, int richtigIndex, Image[] antworten, String path) {
+    private CoverWahlFrage(String fragetext, int richtigIndex, Image[] antworten, String path) {
         super(fragetext, null, richtigIndex);
         this.antworten = antworten;
         this.path = path;
@@ -33,7 +34,7 @@ public class CoverWahlFrage extends Frage {
 
 
     public static CoverWahlFrage generiereFrage(String path, Image[] alleCover) throws TagNeededException {
-        String titel;
+
         Image srcCover = null;
         ArrayList<Image> antworten = new ArrayList<>();
 
@@ -45,7 +46,6 @@ public class CoverWahlFrage extends Frage {
             if (srctags.getTitle() == null || srctags.getAlbumImage() == null)
                 throw new TagNeededException();
             else {
-                titel = srctags.getTitle();
                 srcCover = SwingFXUtils.toFXImage(ImageIO.read(new ByteArrayInputStream(srctags.getAlbumImage())), null);
             }
 
@@ -57,7 +57,7 @@ public class CoverWahlFrage extends Frage {
         // Antworten generieren
         for (int i = 0; i < 3; i++) {
             Image randAntwort = alleCover[(int) (Math.random() * alleCover.length)];
-            if (!randAntwort.equals(srcCover)) {
+            if (!coverEquals(randAntwort, srcCover)) {
                 boolean add = true;
 
                 for (Image img : antworten) {
@@ -69,8 +69,10 @@ public class CoverWahlFrage extends Frage {
                 }
                 if (add)
                     antworten.add(randAntwort);
-            } else
+            } else {
+                System.out.println("Cover wurden als gleich erkannt.");
                 i--;
+            }
         }
         antworten.add(srcCover);
         Collections.shuffle(antworten);
@@ -91,6 +93,44 @@ public class CoverWahlFrage extends Frage {
 
         return new CoverWahlFrage(fragetext, richtigIndex, antworten.toArray(new Image[antworten.size()]), path);
     }
+
+    /**
+     * Vergleiche pixelweise JavaFX-Images, um doppelte CoverAntworten zu vermeiden.
+     * Hintergrund ist, dass die equals() - Methode nicht die Bilder vergleicht, sondern
+     * Objekt-Identitäten.
+     * <p>
+     * Hierbei wird davon ausgegegangen, dass Cover, deren oberes Viertel gleich ist, als
+     * gleich angesehen werden können, um die Laufzeit zu verringern.
+     *
+     * @param imgA Das erste Bild
+     * @param imgB Das zweite Bild
+     * @return ob die Bilder gleich sind oder nicht.
+     */
+    private static boolean coverEquals(Image imgA, Image imgB) {
+        // Wenn die Bilder ungleich groß sind, können sie nicht gleich sein.
+        if (imgA.getWidth() == imgB.getWidth() && imgA.getHeight() == imgB.getHeight()) {
+            double width = imgA.getWidth();
+            double height = imgA.getHeight();
+
+            PixelReader a = imgA.getPixelReader();
+            PixelReader b = imgB.getPixelReader();
+
+            for (int y = 0; y < height / 2; y++) {
+                for (int x = 0; x < width / 2; x++) {
+                    // Sobald ein pixel ungleich ist, gib false zurück
+                    // System.out.println("Bild 1 " + x + "|" + y + " Pixel" + a.getArgb(x, y) + " == " + b.getArgb(x, y));
+                    if (a.getArgb(x, y) != b.getArgb(x, y)) {
+                        return false;
+                    }
+                }
+            }
+        } else {
+            return false;
+        }
+
+        return true;
+    }
+
 
     public Image getCover(int i) {
         return antworten[i];
